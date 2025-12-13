e0a8d41e7ce4c2b7c4c136860ab839910e9e3bd6
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2025-12-13 13:20:01 -0300

    feature: add HandleInputErrors middleware to manage validation errors for POST requests

---
+++ backend/src/middleware/ValidationsMiddleware.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from "express"
+import { validationResult } from "express-validator"
+
+
+export const HandleInputErrors = ( req : Request , res : Response , nextFunction : NextFunction ) => {
+
+
+    // Handle router POST request validations errors
+
+    let errorsFromRouter_POST = validationResult( req ) ;   // the validation results from the router
+
+    if( !errorsFromRouter_POST.isEmpty() ) {
+
+        return res.status(400).json(
+
+            {
+                errorsDescription: errorsFromRouter_POST.array()
+            }
+
+        )
+    }
+
+    
+    nextFunction() ;
+
+
+}
