aea91c4e8226f70927fe8e25d4124d9d9c3bcf25
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-02 20:19:34 -0300

    feature: refactor CreateAccount and LoginUserAccount functions for improved error handling and password validation ; update README with backend dependencies and add frontend setup instructions

--- backend/src/handlers/HandleRequests.ts
+++ backend/src/handlers/HandleRequests.ts
@@ -2,35 +2,14 @@ import { mongooseUser, userI } from "../modelsToDB/UserModelCollection" ;
 
 import type { Request, Response } from 'express' ;
 
-import { HashingThePassword } from "../authentications/AuthPassword";
+import { CheckTheRespectivePassword, HashingThePassword } from "../authentications/AuthPassword";
 
 import slug from "slug";
 import { errorsMessagesArray } from "../sharedContent/messages/ErorrsMessages";
 import { sucessMessagesArray } from "../sharedContent/messages/SucessMessages";
 
-import { validationResult } from 'express-validator'
-
 
 export const CreateAccount = async ( req : Request , res : Response ) => {
-
-    console.log( '\nPOST Method => From /register via THUNDER CLIENT in VSC \n' )
-
-
-    // Handle router POST request validations errors
-
-    let errorsFromRouter_POST = validationResult( req ) ;   // the validation results from the router
-
-    console.log( errorsFromRouter_POST ) ;
-
-    if( !errorsFromRouter_POST.isEmpty() ) {
-
-        return res.status(400).json( 
-            { 
-                errorsDescription: errorsFromRouter_POST.array()
-            }
-        );
-
-    } 
 
 
     // Destructuring and extraction of the data sent by the client
@@ -41,21 +20,11 @@ export const CreateAccount = async ( req : Request , res : Response ) => {
 
     const handleProfileAlias : string =  req.body.handleProfileAlias
 
-    console.log( email ) ;
 
     const userExists : userI = await mongooseUser.findOne( { email: email } ) ;
 
-    if( userExists ) {
+    if( userExists ) return res.status(409).json( { error: errorsMessagesArray.userAlreadyExists().message } ) ;
 
-        // console.log( `Este usario con email ${userExists.email} ya existe en la BD` ) ;
-
-        return res.status(409).json( { error: errorsMessagesArray.userAlreadyExists().message } ) ;
-
-    }
-
-    else console.log("This user doesn't exist until now")
-
-    
 
 
     // await mongooseUser.create( req.body )    Manera antigua de postear el req.body en la DB ;
@@ -65,32 +34,22 @@ export const CreateAccount = async ( req : Request , res : Response ) => {
 
         //Hashing the password
         
-        const theHash = await HashingThePassword( password ) ;
+        userToAdd.password = await HashingThePassword( password ) ;
 
-        console.log( `the ${req.body.name} password hashed is ${theHash}`) ;
-
-        userToAdd.password = theHash ;
-
-        // Optimizado:  userToAdd.password = await HashingThePassword( password ) ;
 
         // Clean the handleProfileAlias into a friendly URL
 
         const cleanHandleProfileAlias : string = slug( handleProfileAlias, '' ) ;
 
-        if( cleanHandleProfileAlias ){
+        const cleanHPA_AlreadyExist = await mongooseUser.findOne( { handleProfileAlias: cleanHandleProfileAlias } ) ;
+        
+        if( cleanHPA_AlreadyExist ) return res.status(409).json( { error: errorsMessagesArray.userAlreadyExists('handleProfileAlias').message} )
 
-            const cleanHP_AlreadyExist = await mongooseUser.findOne( { handleProfileAlias: cleanHandleProfileAlias } ) ;
 
+        userToAdd.handleProfileAlias = cleanHandleProfileAlias ;
 
-            return res.status(409).json( { error: errorsMessagesArray.userAlreadyExists('handleProfileAlias').message} )
+        //console.log( cleanHandleProfileAlias) );
 
-        }
-
-        userToAdd.handleProfileAlias = cleanHandleProfileAlias
-
-        console.log( slug(handleProfileAlias, '') );
-
-        //userToAdd.handleProfileAlias = slug( handleProfileAlias, '' )
 
     await userToAdd.save() ;
 
@@ -104,31 +63,15 @@ export const CreateAccount = async ( req : Request , res : Response ) => {
 
 export const LoginUserAccount = async ( req : Request , res : Response  ) => {
 
-    console.log( 'Desde login...' )
-
-    // Handle router POST request validations errors, for login
-
-    let errorsFromRouter_POST = validationResult( req ) ;
-
-    if( !errorsFromRouter_POST.isEmpty() ) {
-
-        return res.status(400).json(
-            {
-                errorsDescription: errorsFromRouter_POST.array()
-            }
-        )
-
-    }
-
 
     const email : string = req.body.email ;
 
     const password : string = req.body.password ;
 
 
-    const userIsLogged : userI = await mongooseUser.findOne({ email }) ;
+    const loggedUser : userI = await mongooseUser.findOne({ email }) ;
 
-    if( !userIsLogged ) {
+    if( !loggedUser ) {
 
         return res.status(404).json(
             {
@@ -136,6 +79,29 @@ export const LoginUserAccount = async ( req : Request , res : Response  ) => {
             }
         )
     }
+
+
+    // Check the respective password
+
+
+    const isPasswordCorrect = await CheckTheRespectivePassword( password, loggedUser.password ) // Escribir en tapa de cuaderno que se no pusiera el = await... 
+    //  la const isPasswordCorrect seria del mismo tipo Promise<boolean> que la function, en vez de boolean (optimo para el if)
+
+    if( !isPasswordCorrect ) {
+
+        return res.status(401).json(
+            {
+                errorPassword: errorsMessagesArray.unmatchedPassword.message
+            }
+        )
+
+    }
+
+    res.status( 200 ).json({
+
+        sucessLogin: sucessMessagesArray.loginCorrect
+
+    })
 
 
 }
