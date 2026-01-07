c63240c7bf4b47f54afbd04ca0dfd0305107034f
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-07 18:04:04 -0300

    feature: create TheRouter component to manage application routes using react-router | implement routing with TheRouter component  | Update main.tsx to render TheRouter instead of App component.

---
+++ frontend/src/Router.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom' ;
+
+
+export default function TheRouter () {
+
+    return(
+
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route>
+
+                    <Route></Route>
+                    
+                    <Route></Route>
+
+                </Route>
+
+            </Routes>
+
+        </BrowserRouter>
+
+    )
+
+
+}
