6fed50667f480968199ebe010ca08df04040445e
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-08 15:27:08 -0300

    feature: add the new LoginView component and implement it with navigation to registration

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,1 +1,25 @@
-import {} from 'react-hook-form' ;
+import { Link } from 'react-router-dom' ;
+
+export default function LoginView () {
+
+    return(
+
+        <div>
+            <h2 className="text-4xl text-white font-bold">Login</h2>
+            
+            <nav className="mt-10">
+
+                <Link to="/auth/register">
+
+                    <button>
+                        Dont have a account already? Register
+                    </button>
+                
+                </Link>
+
+            </nav>
+        </div>
+
+    )
+
+}
