a474749dc0010eef7e16d205c0595df47d8c9ac2
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-09 12:25:10 -0300

    feature: add CORS configuration to handle allowed origins for cross-origin requests and implement it through appServer.use() hook in Server.ts

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -3,12 +3,23 @@ import 'dotenv/config' ;
 import theRouter from './TheRouter';
 import { ConnectToMyMongoDB } from './connectionsConfig/MyDB';
 
+import cors from 'cors'
+
+
+import { CORSconfig } from './connectionsConfig/CorsConfig';
 
 const appServer = express() ;
 
 
 ConnectToMyMongoDB() ;
 
+
+//CORS
+
+appServer.use(cors(CORSconfig))
+
+
+// Access to process the POST requests with JSON format
 
 appServer.use( express.json() ) ;
 
