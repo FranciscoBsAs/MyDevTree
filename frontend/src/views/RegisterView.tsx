cd9d5665a1d3920b250fb90e462d8869fab2210c
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-09 12:37:17 -0300

    feature: implement async registration, POST request with input register data, handling with axios and ToastService ad for improved user feedback

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -3,6 +3,10 @@ import { useForm } from "react-hook-form";
 import { ErrorMessageComponent } from "../components/ErrorMessageComponent";
 import type { userRegisterFrontI } from "../interfaces/UserInterfaces";
 import { errorsMessageObj } from "../sharedFrontContent/messagesArray/FrontErrorsMessages";
+import axios from "axios";
+import { ToastService } from "../sharedFrontContent/alertToasts/ToastService";
+
+
 
 export default function RegisterView () {
 
@@ -21,14 +25,32 @@ export default function RegisterView () {
   const { register, watch, handleSubmit, formState: {errors} } = useForm< userRegisterFrontI >( { defaultValues: initialValues } ) ;
 
 
-  if(errors) console.error( errors ) ;
+  //if(errors) console.error( errors ) ;
 
 
   const inputPassword : string = watch('password') ;
 
 
-  const handleRegister = ( inputFormData : userRegisterFrontI ) => {
-      console.log( 'Desde handleRegister' )
+  const handleRegister = async ( inputFormData : userRegisterFrontI ) => {
+
+    try {
+
+      const response = await axios.post('http://localhost:4000/Root/auth/register', inputFormData ) ;
+
+      // const {data} = await axios.post('http://localhost:4000/Root/auth/register', inputFormData ) ;
+
+      //if( response )console.log(response)
+
+      const data = response.data.messageSucess ;
+
+      if(data) ToastService.success( data ) ;
+      
+    } catch (error) {
+      
+      ToastService.error( error )
+
+    }
+
   }
 
 
@@ -36,144 +58,145 @@ export default function RegisterView () {
   return (
 
     <div>
+        
 
-      
-      <h2 className="text-2xl text-white font-bold">Made a account</h2>
+        <h2 className="text-2xl text-white font-bold">Made a account</h2>
 
-      <div className="w-full max-w-2xl mx-auto">
+        <div className="w-full max-w-2xl mx-auto">
           
-          <form
-              onSubmit={handleSubmit(handleRegister)}
-              className=" bg-white
-                          px-5 py-5
-                          rounded-lg
-                          space-y-5
-                          mt-2
-              "
-          >
+            <form
+                onSubmit={handleSubmit(handleRegister)}
+                className=" bg-white
+                            px-5 py-5
+                            rounded-lg
+                            space-y-5
+                            mt-2
+                "
+            >
 
-              {/*(div.grid.grid-cols-1.space-y-3>label.text-1xl.text-slate-500+input.bg-slate-100.border-none.p-2.rounded-lg.placeholder-slate-400[id=x type=text placeholder=x])*5 */}
+                {/*(div.grid.grid-cols-1.space-y-3>label.text-1xl.text-slate-500+input.bg-slate-100.border-none.p-2.rounded-lg.placeholder-slate-400[id=x type=text placeholder=x])*5 */}
 
-              <div className="grid grid-cols-1 space-y-3">
+                <div className="grid grid-cols-1 space-y-3">
 
-                  <label htmlFor="name" className="text-1xl text-slate-500">Name</label>
+                    <label htmlFor="name" className="text-1xl text-slate-500">Name</label>
 
-                  <input
-                      type="text"
-                      className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
-                      id="name"
-                      placeholder="Name"
-                      {...register( 'name', {
-                          required: errorsMessageObj.required('name')
-                      } )}
-                  />
+                    <input
+                        type="text"
+                        className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
+                        id="name"
+                        placeholder="Name"
+                        {...register( 'name', {
+                            required: errorsMessageObj.required('name')
+                        } )}
+                    />
 
-                  { errors.name && <ErrorMessageComponent> { errors.name.message } </ErrorMessageComponent> }
+                    { errors.name && <ErrorMessageComponent> { errors.name.message } </ErrorMessageComponent> }
               
-              </div>
+                </div>
 
 
-              <div className="grid grid-cols-1 space-y-3">
+                <div className="grid grid-cols-1 space-y-3">
 
-                  <label htmlFor="email" className="text-1xl text-slate-500">Email</label>
+                    <label htmlFor="email" className="text-1xl text-slate-500">Email</label>
 
-                  <input
-                      type="text"
-                      className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
-                      id="email"
-                      placeholder="Email"
-                      { ...register( 'email', {
-                          required: errorsMessageObj.required('email'),
-                          pattern: {
-                              value: /\S+@\S+\.\S+/,
-                              message: "Email format is invalid"
-                          }
-                      } ) }
-                  />
+                    <input
+                        type="text"
+                        className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
+                        id="email"
+                        placeholder="Email"
+                        { ...register( 'email', {
+                            required: errorsMessageObj.required('email'),
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Email format is invalid"
+                            }
+                        } ) }
+                    />
 
-                  { errors.email && <ErrorMessageComponent> { errors.email.message } </ErrorMessageComponent> }
+                    { errors.email && <ErrorMessageComponent> { errors.email.message } </ErrorMessageComponent> }
 
-              </div>
+                </div>
 
 
-              <div className="grid grid-cols-1 space-y-3">
+                <div className="grid grid-cols-1 space-y-3">
 
-                  <label htmlFor="handleProfileAlias" className="text-1xl text-slate-500">Handle Profile Alias</label>
+                    <label htmlFor="handleProfileAlias" className="text-1xl text-slate-500">Handle Profile Alias</label>
 
-                  <input
-                      type="text"
-                      className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
-                      id="handleProfileAlias"
-                      placeholder="A user profile alias for the URLs | NoSpacesAllowed"
-                      {...register( 'handleProfileAlias' , {
-                          required: errorsMessageObj.required('handle profile alias')
-                      } ) }
-                  />
+                    <input
+                        type="text"
+                        className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
+                        id="handleProfileAlias"
+                        placeholder="A user profile alias for the URLs | NoSpacesAllowed"
+                        {...register( 'handleProfileAlias' , {
+                            required: errorsMessageObj.required('handle profile alias')
+                        } ) }
+                    />
 
-                  { errors.handleProfileAlias && <ErrorMessageComponent> { errors.handleProfileAlias.message } </ErrorMessageComponent> }
+                    { errors.handleProfileAlias && <ErrorMessageComponent> { errors.handleProfileAlias.message } </ErrorMessageComponent> }
                   
-              </div>
+                </div>
 
 
-              <div className="grid grid-cols-1 space-y-3">
+                <div className="grid grid-cols-1 space-y-3">
 
-                  <label htmlFor="password" className="text-1xl text-slate-500">Password</label>
+                    <label htmlFor="password" className="text-1xl text-slate-500">Password</label>
 
-                  <input
-                      type="text"
-                      className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
-                      id="password"
-                      placeholder="Password"
-                      { ...register( 'password', {
-                          required: errorsMessageObj.required('password')
-                      } ) }
-                  />
+                    <input
+                        type="text"
+                        className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
+                        id="password"
+                        placeholder="Password"
+                        { ...register( 'password', {
+                            required: errorsMessageObj.required('password')
+                        } ) }
+                    />
 
-                  { errors.password && <ErrorMessageComponent> {errors.password.message} </ErrorMessageComponent> }
+                    { errors.password && <ErrorMessageComponent> {errors.password.message} </ErrorMessageComponent> }
 
-              </div>
+                </div>
 
 
-              <div className="grid grid-cols-1 space-y-3">
+                <div className="grid grid-cols-1 space-y-3">
 
-              <label htmlFor="password_confirmation" className="text-1xl text-slate-500">
-                      Password Confirmation
-              </label>
+                    <label htmlFor="password_confirmation" className="text-1xl text-slate-500">
+                        Password Confirmation
+                    </label>
 
-              <input
-                  type="text"
-                  className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
-                  id="password_confirmation"
-                  placeholder="Password"
-                  { ...register( 'password_confirmation', {
-                      required: errorsMessageObj.required('password confirmation'),
-                      validate: (inputValue) => (
-                        inputValue === inputPassword 
-                                      ? true 
-                                      : errorsMessageObj.notTheSamePassword
-                      )
-                  } ) }
-              />
+                    <input
+                        type="text"
+                        className="bg-slate-100 border-none p-2 rounded-lg placeholder-slate-400"
+                        id="password_confirmation"
+                        placeholder="Password"
+                        { ...register( 'password_confirmation', {
+                            
+                            required: errorsMessageObj.required('password confirmation'),
+                            validate: (inputValue) => (
+                            inputValue === inputPassword 
+                                        ? true 
+                                        : errorsMessageObj.notTheSamePassword
+                            )
+                        } ) }
+                    />
 
-              { errors.password_confirmation && <ErrorMessageComponent> { errors.password_confirmation.message } </ErrorMessageComponent> }
+                    { errors.password_confirmation && <ErrorMessageComponent> { errors.password_confirmation.message } </ErrorMessageComponent> }
               
-              </div>
+                </div>
 
 
-              <input type="submit" className="bg-cyan-700 p-3 text-lg w-full text-slate-200 rounded-lg font-bold cursor-pointer" value="Make account" />
+                <input type="submit" className="bg-cyan-700 p-3 text-lg w-full text-slate-200 rounded-lg font-bold cursor-pointer" value="Make account" />
           
 
-          </form>
+            </form>
       
       
-      </div>
+        </div>
     
     
-      <nav className="mt-10 text-3xl text-blue-500" >
-          <Link to='/auth/login' >
-                  <button>Already have a account? Login</button>
-          </Link>
-      </nav>
+        <nav className="mt-10 text-3xl text-blue-500" >
+            <Link to='/auth/login' >
+                <button>Already have a account? Login</button>
+            </Link>
+        </nav>
 
     </div>
 
