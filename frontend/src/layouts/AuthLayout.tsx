628b3924f3ea2ecca5914212fa3893d8b4b3c063
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-09 16:40:31 -0300

    feature: fix layout structure in AuthLayout with the correct line for the <Outlet/>

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -8,31 +8,27 @@ export default function AuthLayout () {
 
 
         <div className="bg-slate-700 min-h-screen">
+
             <Toaster position="top-center"/>
 
-            <div
-                className=' bg-slate-800 
-                            h-16 flex 
-                            items-center' 
-            >
-
+            <div className=' bg-slate-800 h-16 flex items-center'>
 
                 <div className="bg-slate-800 h-16 flex items-center">
 
                     <div className="max-w-7x1 mx-auto px-5 h-full flex items-center">
-                        <img src="/myDevTree_Logo.jpg" alt="Logotipo MyDevTree" className="h-14" />
+                        <img src="/myDevTree_Logo.jpg" alt="Logotipo MyDevTree" className="h-14" ></img>
                     </div>
 
                     <div className="py-9">
-                        <h1 className="text-white"></h1>
+                        <h1 className="text-white">Dev Tree web site</h1>
                     </div>
 
                 </div>
 
-                <Outlet/>
-
             </div>
 
+            <Outlet/>
+        
         </div>
 
 
