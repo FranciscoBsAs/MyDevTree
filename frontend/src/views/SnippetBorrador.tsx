049ce58683a0afd4566541ff77029f058b5a2e20
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-10 20:02:12 -0300

    feature: add TSX component as BORRADOR

---
+++ frontend/src/views/SnippetBorrador.tsx
@@ -0,0 +1,27 @@
+import { useForm } from 'react-hook-form'
+import { ErrorMessageComponent } from '../components/ErrorMessageComponent'
+
+
+export const SnippetBorrador = () => {
+
+
+    const { register, formState:{errors} } = useForm()
+
+    return(
+
+//            form.bg-white.px-5.py-20.rounded-lg.space-y-10.mt-10[onSubmit={()=>{}} noValidate={true}]>(div.grid.grid-cols-1.space-y-3>label.text-2xl.text-slate-500+input.bg-slate-100.border-none.p-3.rounded-lg.placeholder-slate-400[id=x type=x placeholder=x ]+{{errors.x && <ErrorMessageComponent></ErrorMessageComponent>}})*2
+
+
+        <div>
+
+            <form action="" className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10" onSubmit={()=>{}} noValidate={true}>
+                <div className="grid grid-cols-1 space-y-3"><label htmlFor="" className="text-2xl text-slate-500"></label><input type="x" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" id="x" placeholder="x" />{errors.x && <ErrorMessageComponent>{'h'}</ErrorMessageComponent>}</div>
+                <div className="grid grid-cols-1 space-y-3"><label htmlFor="" className="text-2xl text-slate-500"></label><input type="x" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" id="x" placeholder="x" />{errors.x && <ErrorMessageComponent>{'g'}</ErrorMessageComponent>}</div>
+            </form>
+
+
+        </div>
+
+    )
+
+}
