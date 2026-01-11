0a933b85fd8c98eb2ee824e89916b7b9a6a45274
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-11 13:12:48 -0300

    feature: delete the async in toastService success prop

--- frontend/src/sharedFrontContent/alertToasts/ToastService.ts
+++ frontend/src/sharedFrontContent/alertToasts/ToastService.ts
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast' ;
 
 export const toastService = {
 
-    success: async ( message : string ) => {
+    success: ( message : string ) => {
 
         toast.success( message ) 
 
