fa54fefa7aa4452ed77ec06d363458c2e928ddf3
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-11 13:12:48 -0300

    feature: add dev:api script to facilitate API development and adjust CORS allowed origins with process.argv conditions

--- backend/src/connectionsConfig/CorsConfig.ts
+++ backend/src/connectionsConfig/CorsConfig.ts
@@ -1,13 +1,15 @@
 import { CorsOptions } from 'cors'
 
-
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5173/'];     // white list
+// white list
+const allowedOrigins = [process.env.MEMPHIS_URL1, process.env.MEMPHIS_URL2]; 
 
 
 
 export const CORSconfig : CorsOptions = {
 
     origin: ( origin, callback ) => {
+
+        if( process.argv[2] === '--api' ) allowedOrigins.push(undefined) ;
 
         if( !origin || allowedOrigins.includes(origin) ) callback(null, true) ;
 
