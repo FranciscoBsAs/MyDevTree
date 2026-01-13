5cbdb73ae8e9088c740889ee9ee2adeb5cae602f
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-13 19:41:24 -0300

    feature: fix the name atributed in SucessMessages object

--- backend/src/sharedContent/messages/SucessMessages.ts
+++ backend/src/sharedContent/messages/SucessMessages.ts
@@ -1,7 +1,4 @@
 export const sucessMessagesArray = {
-
     registerCorrect: "Register has been created correctly",
-
-    loginCorrect1: "Correct login for the user"
-
+    loginCorrect: "Correct login for the user"
 }
