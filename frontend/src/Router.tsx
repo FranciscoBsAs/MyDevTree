5aa1af868cc9f1a969585e5e06f345b1d655ec93
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-15 21:12:47 -0300

    feature: add routing for admin section with LinkTreeView and ProfileView components

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,7 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom' ;
-import AuthLayout from './layouts/AuthLayout';
-import LoginView from './views/LoginView';
-import RegisterView from './views/RegisterView';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginView from './views/LoginView'
+import RegisterView from './views/RegisterView'
+import AuthLayout from './layouts/AuthLayout'
+import AppLayout from './layouts/AppLayout'
+import LinkTreeView from './views/LinkTreeView'
+import ProfileView from './views/ProfileView'
 
 
 export default function TheRouter () {
@@ -24,6 +27,22 @@ export default function TheRouter () {
                     ></Route>
 
                 </Route>
+
+
+                <Route path='/admin' element={<AppLayout></AppLayout>} >
+
+                    <Route
+                        index={true} element={<LinkTreeView></LinkTreeView>}
+                    >
+                    </Route>
+
+                    <Route
+                        path='profile' element={<ProfileView></ProfileView>}    //      relative path: /admin/profile  NOT ABSOLUTE PATH
+                    >
+                    </Route>
+
+                </Route>
+
 
             </Routes>
 
