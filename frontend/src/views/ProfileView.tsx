f36ef9275f2cda15e3ec8293b786174ef0150164
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-18 12:36:22 -0300

    feature: enhance ProfileView with form fields for alias, description, and image upload

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -1,10 +1,77 @@
 export default function ProfileView () {
 
-    return(
-    
+    return (
+
         <div>
-            Hi from ProfileView
+
+            <h3 className="text-2xl text-white font-bold">Edit Profile</h3>
+
+
+            <div className="w-full max-w-2xl mx-auto" >
+
+                <form
+                    action=""
+                    className="bg-white px-5 py-5 rounded-lg space-y-5 mt-2"
+                    onSubmit={() => {}}
+                >
+
+                    <legend className="text-2xl text-slate-800 text-center">
+                        Edit Profile Information
+                    </legend>
+
+                    <div className="grid grid-cols-1 gap-2   space-y-3 ">
+
+                        <label htmlFor="">Handle Profile Alias</label>
+                        <input
+                            type="text"
+                            className="border-none bg-slate-100 rounded-lg p-2  placeholder-slate-400 "
+                            placeholder="handle profile alias or user name"
+                        />
+                    </div>
+
+                    
+                    <div className="grid grid-cols-1 gap-2">
+
+                        <label htmlFor="">Description</label>
+                        <textarea
+                            name=""
+                            id=""
+                            className="border-none bg-slate-100 rounded-lg p-2  placeholder-slate-400"
+                            placeholder="Your description"
+                        ></textarea>
+                    </div>
+
+
+                    <div className="grid grid-cols-1 gap-2">
+
+                        <label htmlFor="">Image</label>
+
+                        <input
+                            type="file"
+                            className="border-none bg-slate-100 rounded-lg p-2  placeholder-slate-400"
+                            id="image"
+                            name="handle"
+                            accept="image/*"
+                            onChange={() => {}}
+                        />
+
+                    </div>
+
+
+                    <input
+                        type="text"
+                        //className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                        className="bg-cyan-400 p-3 text-lg w-full text-slate-200 rounded-lg font-bold cursor-pointer"
+                        value="Save changes"
+                    />
+
+
+                </form>
+
+            </div>
+            <br/>
         </div>
-    
+        
     )
+
 }
