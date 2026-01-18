5a6e7b66b8e9df2de36e4a1fe84ed00bdf9219cb
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-18 12:36:36 -0300

    feature: refactor AppLayout component to clean up unused code and improve structure

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -8,15 +8,8 @@ export default function AppLayout () {
 
     return(
 
-
-        //div>((header.bg-slate-800.py-5>div.mx-auto.max-w-5xl.flex.flex-col.md:flex-row.items-center.md:justify-between>(div.w-full.p-5.lg:p-0.md:w-1/3>img.w-full.block[src=/myDevTree_Logo.jph])+div.md:w-1/3.md:flex.md:justify-end>button.bg-lime-500.p-2.text-slate-800.uppercase.front-black.text-xs.rounded-lg.cursor-pointer[onClick={{}}])+div.bg-gray-100.min-h-screen.py-10>main.mx-auto.max-w-5xl.p-10.md:p-0)+div.flex.justify-end
-
-
         // Real Emmet snippet:
             //div>(header.bg-slate-800.py-5>div.mx-auto.max-w-5xl.flex.flex-cols.md:flex-row.items-center.md:justify-between>(div.w-full.p-5.lg:p-0.md:w-1/3>img.w-full.blocl[src=/myDevTree_Logo.jpg])+(div.md:w-1/3.md:flex.md:justify-end>(button.bg-line-500.p-2.text-slate-800.uppercase.front-black.text-xs.rounded-lg.cursor-pointer[onClick={}]>{LogOut})))+(div.bg-gray-100.min-h-screen.py-10>main.mx-auto.max-w-5xl.p-10.md:p-0>div.flex.justify-end>Link.font-bold.text-right.text-slate-800.text-2xl[to={''} target="_blank" rel="noreferrer noopener"]>{Visit my Profile})+((div.flex.flex-col.md:flex-row.gap-10.mt-10>div.flex-1>Outlet)+div.w-full.md:w-96.bg-slate-800.px-5.py-10.space-y-6)+Toaster[position="top-right"]
-
-
-
 
 
         <div className="bg-slate-700 min-h-screen" >
@@ -57,24 +50,19 @@ export default function AppLayout () {
                         </Link>
                     </div>
 
+
                 </main>
-
+                
             </div>
-
 
             <div className="flex flex-col md:flex-row gap-10 mt-10">
 
                 <div className="flex-1">
                     <Outlet/>
                 </div>
+                <div className="w-full md:w-96 bg-slate-800 px-5 py-10 space-y-6"></div>
 
             </div>
-
-            <div className="w-full md:w-96 bg-slate-800 px-5 py-10 space-y-6">
-
-            </div>
-            
-
 
 
         </div>
