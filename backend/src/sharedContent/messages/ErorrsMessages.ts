9057fba33d589ef9e58dda99846eceb9604426d1
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-20 13:52:24 -0300

    feature: add error messages for unauthorized user session and non-existent user

--- backend/src/sharedContent/messages/ErorrsMessages.ts
+++ backend/src/sharedContent/messages/ErorrsMessages.ts
@@ -53,6 +53,12 @@ export const errorsMessagesArray = {
 
     unmatchedPassword: new Error('The password does not match the provided email') 
 
+    ,
 
+    noAuthorizeUser: new Error('User session is not authorized')
+
+    ,
+
+    userNotExist: new Error("The user doesn't exist ")
 
 }
