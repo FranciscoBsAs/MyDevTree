7f06ccc2838a4e4950fda62fb87ddc7c84b2193f
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-21 18:27:30 -0300

    feature: add request interceptor to Axios instance for authorization token

--- frontend/src/configConnection/AxiosInstance.ts
+++ frontend/src/configConnection/AxiosInstance.ts
@@ -1,8 +1,23 @@
-import axios, { type AxiosInstance } from "axios";
+import axios, { type AxiosInstance, type InternalAxiosRequestConfig } from "axios";
 
 const APIaxiosInstance : AxiosInstance = axios.create(
     {
         baseURL: import.meta.env.VITE_MEMPHIS_BACK
+    }
+)
+
+APIaxiosInstance.interceptors.request.use(
+
+    ( config : InternalAxiosRequestConfig ) => {
+
+        const token : string | null = localStorage.getItem( 'AUTH_TOKEN' )
+
+
+        if(token) config.headers.Authorization = `Bearer ${token}`  ;
+
+
+        return config
+
     }
 )
 
