2e139901eb172be720d5fd4812c021e4e97d73d5
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-21 18:26:05 -0300

    feature: integrate React Query and React Query Devtools into main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TheRouter from './Router.tsx'
+import TheRouter from './Router' ;
+import { QueryClient , QueryClientProvider } from '@tanstack/react-query' ;
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools' ;
+
+
+const theQueryClient = new QueryClient ;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TheRouter />
+    <QueryClientProvider client={theQueryClient} >
+      < TheRouter />
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
   </StrictMode>,
 )
