552a5e1f5263e531b89e9964acd1587a675bc4cf
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-21 18:26:34 -0300

    feature: add GetUserFetching function and user query configuration

---
+++ frontend/src/api/GetUserFetching_useQuery.ts
@@ -0,0 +1,31 @@
+import toastService from "../sharedFrontContent/alertToasts/ToastService";
+import APIaxiosInstance from "../configConnection/AxiosInstance";
+import { isAxiosError } from 'axios'
+import type { userFrontI } from "../interfaces/UserInterfaces";
+
+
+export async function GetUserFetching () {
+
+    const token : string | null = localStorage.getItem( 'AUTH_TOKEN' )
+
+    try {
+        
+        const { data } = await APIaxiosInstance.get<userFrontI>( '/admin/profile' )
+
+        //console.table(data)
+
+        return data ;
+
+    } catch (error) {
+
+        if( isAxiosError(error) && error.response ) {
+            
+            toastService.error(error.response.data.error) ;
+
+            throw new Error( error.response.data.error )
+
+        }
+
+    }
+
+}
