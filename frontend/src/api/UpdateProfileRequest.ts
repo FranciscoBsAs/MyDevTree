4895b28cf4ab3b4ea7e685e8dff31d33551716c6
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-23 18:33:05 -0300

    feature: implement UpdateProfileRequest and useMutation configuration for integrate these with ProfileView for user profile edit/updates

---
+++ frontend/src/api/UpdateProfileRequest.ts
@@ -0,0 +1,21 @@
+import { isAxiosError } from "axios";
+import APIaxiosInstance from "../configConnection/AxiosInstance";
+import type { updateProfileResponseI, userEditFrontI, userFrontI } from "../interfaces/UserInterfaces";
+
+
+export async function UpdateProfileRequest ( formData : userEditFrontI ) {
+
+    try {
+        
+        const { data } = await APIaxiosInstance.patch<updateProfileResponseI>('/admin/profile', formData)
+
+        return data ;
+
+
+    } catch (error) {
+
+        if( isAxiosError(error) && error.response ) throw new Error( error.response.data.error ) ;
+        
+    }
+
+}
--- frontend/src/assets/UserQueryConfig.ts
+++ frontend/src/assets/UserQueryConfig.ts
@@ -1,6 +1,9 @@
-import { useQuery, type DefinedUseQueryResult, type UseQueryOptions } from '@tanstack/react-query' ;
-import type { userFrontI } from '../interfaces/UserInterfaces';
+import { QueryClient, useQuery, type DefinedUseQueryResult, type UseMutationOptions, type UseQueryOptions } from '@tanstack/react-query' ;
+import type { updateProfileResponseI, userEditFrontI, userFrontI } from '../interfaces/UserInterfaces';
 import { GetUserFetching } from '../api/GetUserFetching_useQuery';
+import { UpdateProfileRequest } from '../api/UpdateProfileRequest';
+import toastService from '../sharedFrontContent/alertToasts/ToastService';
+
 
 
 export const getUser_ConfigQuery : UseQueryOptions< userFrontI | undefined, Error > = {
@@ -14,3 +17,60 @@ export const getUser_ConfigQuery : UseQueryOptions< userFrontI | undefined, Erro
     refetchOnWindowFocus: false
 
 }
+
+export const useMutation_ConfigQuery = ( someQueryClient : QueryClient ) : UseMutationOptions< // Tabular en production
+        updateProfileResponseI | undefined, // Lo que RETORNA la API
+        Error,              // Tipo de error
+        userEditFrontI      // Lo que finalmente se envia a la API
+> => {
+
+    return (
+        {
+            mutationFn: UpdateProfileRequest ,
+
+            onError: ( error : Error ) => toastService.error('This error happend while the user profile was updating:  '+ error.message.toUpperCase),
+
+            onSuccess: ( data ) => {
+                
+                toastService.success(`${data  ?  data.messageSucess  :  'Profile updated successfully' } `)
+
+                someQueryClient.invalidateQueries(
+                    {
+                        queryKey: ['user']
+                    }
+                )
+
+            }
+        }
+    )
+
+}
+
+
+// UseMutationOptions< userEditFrontI | undefined, Error, userEditFrontI >
+/**
+ * userEditFrontI | undefined (TData) - El tipo de dato que retorna la mutación cuando tiene éxitoEs lo que recibirás en onSuccess(data). Tu API puede retornar un userEditFrontI o undefined. 
+ * Error (TError) - El tipo de error que puede ocurrir => Es lo que recibirás en onError(error)
+ * userEditFrontI (TVariables) - El tipo de las variables/parámetros que envías a la función de mutación => Es lo que pasas cuando ejecutas la mutación: mutate(userEditFrontI) . Se envía a mutationFn: UpdateProfileRequest
+Básicamente: recibes userEditFrontI → envías a la API → obtienes de vuelta userEditFrontI | undefined o un Error.
+ */
+
+
+/*  EN PRODUCTION
+
+
+export const useMutation_ConfigQuery = (queryClient: QueryClient) : UseMutationOptions<updateProfileResponseI | undefined,Error,userEditFrontI > => ({ RETURN de OBJECT IMPLICITO ({})
+    mutationFn: UpdateProfileRequest,
+
+    onError: (error: Error) => toastService.error('This error happend while the user profile was updating: ' + error.message.toUpperCase()),
+
+    onSuccess: (data) => {
+        toastService.success(`${data ? data.messageSucess : 'Profile updated successfully'}`)
+        
+        // Invalida la query para refrescar los datos
+        queryClient.invalidateQueries({ queryKey: ['user'] })
+    }
+})
+
+
+*/
