9cd044d27d026fc8fce3a28f77113027a9b416ba
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-23 23:22:09 -0300

    feature: implement profile update and image upload functionalities with Cloudinary integration and Formidable functionalities

--- backend/src/handlers/HandleRequests.ts
+++ backend/src/handlers/HandleRequests.ts
@@ -6,6 +6,9 @@ import { errorsMessagesObject } from "../sharedContent/messages/ErorrsMessages";
 import { sucessMessagesArray } from "../sharedContent/messages/SucessMessages";
 import { generateJWT } from "../authentications/jwt";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import formidable from "formidable";
+import cloudinary from "../connectionsConfig/Cloudinary";
+import {UploadApiOptions} from 'cloudinary'
 
 
 export const CreateAccount = async ( req : Request , res : Response ) => {
@@ -106,10 +109,105 @@ export const LoginUserAccount = async ( req : Request , res : Response  ) => {
 
 
 
-export const getUser = async ( req : Request, res : Response ) => {
+export const GetUser = async ( req : Request, res : Response ) => {
 
     //console.log("Testing from getUser handle function");
 
     res.status(200).json( req.user ) ;
+
+}
+
+
+export const UpdateProfile = async ( req : Request , res : Response ) => {
+
+    try {
+
+        const { description } = req.body ;
+
+
+        const cleanHandleProfileAlias : string = slug( req.body.handleProfileAlias ) ;
+
+        const userWithCleanHP = await mongooseUser.findOne({handleProfileAlias: cleanHandleProfileAlias }) ;
+
+        if( userWithCleanHP  &&  userWithCleanHP?.email !== req.user.email ) return res.status(409).json( { error:errorsMessagesObject.userAlreadyExists('Handle Profile Alias').message } ) ;
+
+
+
+        req.user.description = description ;
+
+        req.user.handleProfileAlias = cleanHandleProfileAlias
+
+
+        await req.user.save() ;
+
+
+        res.status(201).json( { messageSucess: sucessMessagesArray.registerCorrect } ) ;
+        
+
+    } catch (e) {
+
+        return res.status(500).json( { error: errorsMessagesObject.updateError } )
+        
+    }
+
+}
+
+
+
+export const UploadImage = async ( req : Request, res : Response ) => {
+
+    
+    try {
+
+        console.log( '\ndesde UploadImage' )
+        
+        formidable( {multiples: false} )?.parse( req , ( error, fields, files ) => {
+    
+            console.log('2. TOTAL de Files recibidos:', files.length);
+            
+            // Validate if the certain file exist
+            if (!files.file || !files.file[0]) return res.status(400).json({ error: errorsMessagesObject.notImageUploadAlready }) ;
+            
+
+
+            const validMimeTypes = ['image/jpeg', 'image/png', 'image/jpg']
+
+            if(  !validMimeTypes.includes( files.file[0].mimetype || '' )  ) return res.status(400).json( {error: errorsMessagesObject.invalidImageFormat.message } ) ;
+
+
+            const uploaderImageFormat_Config : UploadApiOptions = { 
+                allowed_formats: ['jpg', 'png'] ,
+                resource_type: 'image',
+                public_id: `${req.user.handleProfileAlias}-image-${req.user.handleProfileAlias.length}`
+            };
+
+            cloudinary.uploader.upload( files.file[0].filepath, uploaderImageFormat_Config, async ( error, callResult ) => {
+
+                if( error ) return res.status(500).json({error}) ;
+
+                
+                req.user.imageURL = callResult.secure_url ;
+
+                await req.user.save() ;
+                
+                return res.status(200).json( 
+                    { 
+                        message: `Image ${callResult?.original_filename}.${callResult?.format} uploaded successfully`,
+                        imageURL: callResult?.secure_url 
+                    }
+                ) ;
+                
+
+            } )
+    
+        } )
+
+
+        
+    } catch (e) {
+        
+        return res.status(500).json( { error: errorsMessagesObject.updateError } )
+
+    }
 
 }
