dcce5e33b7f942015c2a968fa1114ebe9b4cd803
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-23 23:21:26 -0300

    feature: extend userI interface to inherit from Document and adjust imageURL property

--- backend/src/modelsToDB/UserModelCollection.ts
+++ backend/src/modelsToDB/UserModelCollection.ts
@@ -1,7 +1,7 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
 
-export interface userI {
+export interface userI extends Document {
 
     name : string ,
 
@@ -11,9 +11,11 @@ export interface userI {
 
     handleProfileAlias : string ,
 
-    _id? : string ,
+    //_id? : mongoose.Types.ObjectId | string ,
 
-    description? : string
+    description? : string ,
+
+    imageURL? : string
 
 
 
@@ -69,6 +71,14 @@ const userSchema = new Schema(
             type: String,
 
             default: '',
+
+        },
+
+        imageURL: {
+
+            type: String,
+
+            default: '' ,
 
         }
 
