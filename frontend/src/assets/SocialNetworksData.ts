a0f1057871f4db01f60dfe3f9d2dcf658d38834d
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-26 16:32:07 -0300

    feature: add social networks data and interface for managing social media links

---
+++ frontend/src/assets/SocialNetworksData.ts
@@ -0,0 +1,24 @@
+import type { socialI } from "../interfaces/SocialNetworksInterface";
+
+export const socialNetworksData : socialI[] = [
+
+    { name: 'Facebook', url: '', enabled: false } ,
+
+    { name: 'GitHub', url: '', enabled: false } ,
+
+    { name: 'Instagram', url: '', enabled: false } ,
+
+    { name: 'X', url: '', enabled: false  }, 
+
+    { name: 'YouTube', url: '', enabled: false  }, 
+
+    { name: 'TikTok', url: '', enabled: false  }, 
+
+    { name: 'Twitch', url: '', enabled: false  }, 
+
+    { name: 'LinkedIn', url: '', enabled: true  }, 
+
+    { name: 'personal-web-site', url: '', enabled: false }
+
+
+]
