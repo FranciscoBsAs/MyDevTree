80c33db64adec5dfc4c49e315a8b4c26b5269d06
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-26 16:41:30 -0300

    feature: implement DevTreeInput component with Swith component from headlessui and 3 props in tune with DevTreeInputPropsI interface, and implement the URL validations in LinkTreeView

---
+++ frontend/src/components/DevTreeInput.tsx
@@ -0,0 +1,81 @@
+import type { socialI } from "../interfaces/SocialNetworksInterface"
+import { Switch } from "@headlessui/react"
+import { ClassNames } from "../utils/ClassNames"
+
+
+
+export interface DevTreeInputPropsI {
+
+    item : socialI
+
+    handleUrlChangeProp : ( e : React.ChangeEvent<HTMLInputElement> ) => any ,
+
+
+    handleEnableLinkProp : ( socialNetwork : string ) => void
+
+
+}
+
+
+
+
+export default function DevTreeInput ( { item , handleUrlChangeProp , handleEnableLinkProp } : DevTreeInputPropsI ) {
+
+    const iconImageDirections  = `url('/social/icon_${item.name.toLocaleLowerCase()}${ item.name.toLowerCase() === 'personal-web-site'  ?  '.png'  :  '.svg' }')`
+
+
+    const handleChangeEnable = () => handleEnableLinkProp(item.name) ;
+
+
+    console.log(item)
+
+    return(
+    
+        <div className="bg-slate-400 shadow-sm p-5 flex items-center gap-3  rounded-lg " >
+
+            <div 
+                className="w-12 h-12 bg-cover"
+                style={{ backgroundImage: iconImageDirections }}
+                
+            >
+
+            </div>
+
+            <input 
+
+                type="text" 
+                className="flex-1 border bg-slate-300 border-gray-200 rounded-lg"
+                value={item.url}
+                onChange={handleUrlChangeProp}
+                id={item.name}
+
+
+            />
+
+
+            <Switch
+                checked={ item.enabled }
+                //onChange={ () => {} }
+                onChange={handleChangeEnable}
+                className={ ClassNames( item.enabled  ?  'bg-green-700'  :  'bg-orange-300'  , 
+                    'relative inline-flex h-6 w-11 flex-shirk-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-900 focus:ring-offset-2 '
+                )
+
+                }
+            >
+                <span
+                    aria-hidden="true"
+                    className={ ClassNames(
+                        item.enabled  ?  'translate-x-5'  :  'translate-x-0' ,
+                        'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-slate-100 shadow ring-0 transition duration-200 ease-in-out'
+                    ) }
+                ></span>
+
+
+            </Switch>
+
+
+        </div>
+    
+    )
+}
--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,10 +1,107 @@
+import { socialNetworksData } from "../assets/SocialNetworksData"
+import { useState, type JSX } from "react"
+import DevTreeInput from "../components/DevTreeInput";
+import type { socialI } from "../interfaces/SocialNetworksInterface";
+import { IsValidURL } from "../utils/IsValidURL";
+import toastService from "../sharedFrontContent/alertToasts/ToastService";
+import { errorsMessageObj } from "../sharedFrontContent/messagesArray/FrontErrorsMessages";
+
+
 export default function LinkTreeView () {
+
+
+    const [ devTreeLinks, setDevTreeLinks ] = useState(socialNetworksData) ;
+
+
+
+
+    const handleUrlChange = ( e : React.ChangeEvent<HTMLInputElement> ) => {
+
+        const updatedLinks = devTreeLinks.map( (link) : socialI => {
+
+            const linkUpdated : socialI = {
+                ...link,
+                url: e.target.value
+            }
+
+            if( link.name === e.target.id ) return linkUpdated ;
+
+            else return link ;
+
+        } )
+        //console.log(updatedLinks) ;
+
+
+        setDevTreeLinks( updatedLinks ) ;
+
+    }
+
+
+    const handleEnableLink = ( socialNetwork : string ) => {
+
+        //console.log(socialNetwork) ;
+
+
+        const targetLink = devTreeLinks.find( ( link ) => link.name === socialNetwork )
+
+        if( targetLink && !IsValidURL( targetLink.url ) ) toastService.error( errorsMessageObj.invalidadFormatURL ) ;
+
+
+
+        const updatedLinks = devTreeLinks.map( (link) : socialI => {
+
+
+            const linkUpdated : socialI = {
+            ...link ,
+
+            enabled: !link.enabled
+
+            }
+
+
+            if( link.name === socialNetwork && IsValidURL( link.url ) ) return linkUpdated ;
+
+            else return link
+             
+                
+
+        } )
+
+
+        setDevTreeLinks(updatedLinks)
+
+    }
+
+
+
+
+    console.log(devTreeLinks);
 
     return(
     
         <div>
-            Hi from LinkTreeView
+
+
+
+            <div className="space-y-5">
+
+                { devTreeLinks.map( ( item ) : JSX.Element => (
+
+                    <DevTreeInput
+                        key={item.name}
+                        item={item}
+                        handleUrlChangeProp={handleUrlChange}
+                        handleEnableLinkProp= {handleEnableLink}
+                    />
+
+                ) )}
+ 
+            </div>
+
+
+
         </div>
     
     )
+
 }
