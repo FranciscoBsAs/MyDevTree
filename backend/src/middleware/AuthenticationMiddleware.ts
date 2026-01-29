36ad72ae548f2fe400ef6a7c9ab404865fcef288
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-29 10:42:14 -0300

    feature: fix the name of the errorsMessages Object variable

--- backend/src/middleware/AuthenticationMiddleware.ts
+++ backend/src/middleware/AuthenticationMiddleware.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express"
-import { errorsMessagesArray } from "../sharedContent/messages/ErorrsMessages";
+import { errorsMessagesObject } from "../sharedContent/messages/ErorrsMessages";
 import { sucessMessagesArray } from "../sharedContent/messages/SucessMessages";
 import { generateJWT } from "../authentications/jwt";
 import jwt, { JwtPayload } from "jsonwebtoken";
@@ -26,12 +26,12 @@ export const AuthenticationMiddleware = async ( req : Request , res : Response ,
 
     const bearer = req.headers.authorization ;
 
-    if( !bearer ) return res.status(401).json( { error: errorsMessagesArray.noAuthorizeUser.message } ) ;
+    if( !bearer ) return res.status(401).json( { error: errorsMessagesObject.noAuthorizeUser.message } ) ;
 
 
     const [, token] = bearer.split( ' ' ) ;
 
-    if( !token ) return res.status(401).json( { error: errorsMessagesArray.noAuthorizeUser.message } ) ;
+    if( !token ) return res.status(401).json( { error: errorsMessagesObject.noAuthorizeUser.message } ) ;
 
 
     try {
@@ -43,7 +43,7 @@ export const AuthenticationMiddleware = async ( req : Request , res : Response ,
         const userInMemory = await mongooseUser.findById( result?.iD ).select('-password') ;
 
 
-        if( !userInMemory ) return res.status(401).json( { error: errorsMessagesArray.userNotExist } ) ;
+        if( !userInMemory ) return res.status(401).json( { error: errorsMessagesObject.userNotExist } ) ;
 
 
         //res.json( userInMemory ) ;
