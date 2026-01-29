6a2cd6899f842919d398bcee3bfe84ef7187a81a
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-28 23:01:04 -0300

    feature: update the DevTreeInput and LinkTreeView components for the render cards of social networks and add the IdChecks auxiliar functions

---
+++ frontend/src/utils/IdChecks_Links.ts
@@ -0,0 +1,41 @@
+import type { socialI } from "../interfaces/SocialNetworksInterface";
+
+export function CheckId_Links ( link : socialI, socialNetwork : string, indexToUpdate : number ) : socialI {
+
+    const disabledSocialNetObject : socialI = {
+        ...link,
+        id: 0 ,
+        enabled: false
+    }
+    
+    const updatedItems_Index : socialI = {
+        ...link,
+        id: link.id! -1
+    }
+
+    if( link.name === socialNetwork ) return disabledSocialNetObject ;
+
+    else if( link.id && link.id > indexToUpdate ) return updatedItems_Index ;
+
+    else return link;
+
+}
+
+
+export function OnlyOneIdCheck_Links ( userLinks : socialI[] , socialNetwork : string, continuedId : number  ) {
+
+
+    return userLinks.map( (link) => {
+
+        const socialNetToPersist : socialI = {
+            ...link,
+            enabled: true,
+            id: continuedId
+        }
+
+        return  link.name === socialNetwork  ?  socialNetToPersist  :  link
+            
+    } )
+
+
+}
--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,11 +1,14 @@
 import { socialNetworksData } from "../assets/SocialNetworksData"
-import { useState, type JSX } from "react"
+import { useEffect, useState, type JSX } from "react"
 import DevTreeInput from "../components/DevTreeInput";
 import type { socialI } from "../interfaces/SocialNetworksInterface";
 import { IsValidURL } from "../utils/IsValidURL";
 import toastService from "../sharedFrontContent/alertToasts/ToastService";
 import { errorsMessageObj } from "../sharedFrontContent/messagesArray/FrontErrorsMessages";
-
+import { useMutation , useQueryClient } from "@tanstack/react-query";
+import { useMutation_ConfigQuery } from "../assets/UserQueryConfig";
+import type { userFrontI } from "../interfaces/UserInterfaces";
+import { CheckId_Links, OnlyOneIdCheck_Links } from "../utils/IdChecks_Links";
 
 export default function LinkTreeView () {
 
@@ -13,6 +16,45 @@ export default function LinkTreeView () {
     const [ devTreeLinks, setDevTreeLinks ] = useState(socialNetworksData) ;
 
 
+    const queryClient_Links = useQueryClient()
+
+    const data = queryClient_Links.getQueryData< userFrontI | undefined >(['user'])! ;
+
+    const updateProfileMutation = useMutation( useMutation_ConfigQuery( queryClient_Links ) )
+
+
+    const userLinksFromDB =  ( JSON.parse(data.links!) ) as socialI[]
+
+
+    useEffect( () => {
+
+        const updatedData = devTreeLinks.map( (item) => {
+
+            const userLink = userLinksFromDB.find( ( link ) => link.name === item.name  )
+
+
+            if( userLink ){
+
+                const linkWithUserData = {
+                    ...item ,
+                    url: userLink.url ,
+                    enabled: userLink.enabled
+                }
+
+                return linkWithUserData ;
+
+            } 
+
+            else return item ;
+
+        } )
+
+        setDevTreeLinks( updatedData ) ;
+
+        //console.table(devTreeLinks)
+        //console.table( JSON.parse(data.links!) )
+
+    }, [] )
 
 
     const handleUrlChange = ( e : React.ChangeEvent<HTMLInputElement> ) => {
@@ -55,27 +97,76 @@ export default function LinkTreeView () {
             ...link ,
 
             enabled: !link.enabled
-
             }
 
 
             if( link.name === socialNetwork && IsValidURL( link.url ) ) return linkUpdated ;
 
             else return link
-             
-                
 
         } )
 
 
         setDevTreeLinks(updatedLinks)
 
+
+        const selectedSocialNetwork = updatedLinks.find( (link) => link.name === socialNetwork)
+
+        //console.log('\n',selectedSocialNetwork)
+
+
+        let updatedSocialItems : socialI[] = [] 
+
+
+        if( selectedSocialNetwork?.enabled ) {
+
+
+            const isSelectedSocialNetworkInDB = userLinksFromDB.some( link => link.name === socialNetwork ) ; 
+
+            const continuedId = userLinksFromDB.filter( link => link.id!).length + 1 ;
+
+
+            if( isSelectedSocialNetworkInDB ) updatedSocialItems = OnlyOneIdCheck_Links( userLinksFromDB, socialNetwork, continuedId ) ;
+            
+            else{
+
+                let newItem = {
+                    ...selectedSocialNetwork,
+                    id: continuedId
+                }   
+                updatedSocialItems = [ ... userLinksFromDB, newItem ]
+
+            }
+
+        }
+        else{
+            
+            const indexToUpdate = userLinksFromDB.findIndex( (link) => link.name === socialNetwork ) ;
+
+
+            updatedSocialItems = userLinksFromDB.map( (link) => CheckId_Links(link, socialNetwork, indexToUpdate) ) ;
+
+        }
+
+
+                                console.table(updatedSocialItems)
+
+
+        //This go to the DB
+        queryClient_Links.setQueryData( ['user'], ( prevData : userFrontI ) : userFrontI => ({
+                ...prevData ,
+                //links: JSON.stringify( updatedLinks )
+                links: JSON.stringify( updatedSocialItems )
+            })
+
+        )
+
     }
 
 
+    const handleSaveChanges = () => updateProfileMutation.mutate( data! ) ;
 
-
-    console.log(devTreeLinks);
+    
 
     return(
     
@@ -95,10 +186,20 @@ export default function LinkTreeView () {
                     />
 
                 ) )}
+
+
+                <button
+                    className="bg-cyan-200 p-2 text-center text-lg w-full text-slate-700 rounded-lg font-bold "
+                    onClick={handleSaveChanges}
+                >
+                    Save changes
+                </button>
+
  
             </div>
 
 
+            <br/>
 
         </div>
     
