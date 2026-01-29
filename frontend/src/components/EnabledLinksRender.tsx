46adb154febf967a5f60d8c8d5759d3664620844
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-01-28 23:08:20 -0300

    feature: add the components for the render of social-net cards

---
+++ frontend/src/components/EnabledLinksRender.tsx
@@ -0,0 +1,49 @@
+import { useSortable } from "@dnd-kit/sortable"
+import type { socialI } from "../interfaces/SocialNetworksInterface"
+import { CSS } from "@dnd-kit/utilities"
+
+interface enabledLinksRenderPropsI {
+
+    link : socialI
+
+}
+
+
+export default function EnabledLinksRender ( {link} : enabledLinksRenderPropsI ) {
+
+
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable( {
+        id: link.id!
+    })
+
+    const styleDnd = {
+        transform: CSS.Transform.toString( transform ) ,
+        transition
+    }
+
+    const iconImageDirections = `url('/social/icon_${link.name.toLocaleLowerCase()}${ link.name.toLowerCase() === 'personal-web-site'  ?  '.png'  :  '.svg' }')`
+
+
+    return(
+    
+        <li 
+            ref={setNodeRef}
+            style={styleDnd} 
+            className="bg-stone-200 px-2 py-1 flex items-center gap-1 rounded-lg" 
+            {...attributes}
+            {...listeners}
+        >
+            
+
+            <div
+                className="w-9 h-9 bg-cover"
+                style={{ backgroundImage: iconImageDirections }}
+                
+            ></div>
+
+            <p>My <span className="font-bold" >{link.name}</span> </p>
+
+        </li>
+    
+    )
+}
---
+++ frontend/src/components/LinksInProfile.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react"
+import type { userFrontI } from "../interfaces/UserInterfaces"
+import type { socialI } from "../interfaces/SocialNetworksInterface"
+import EnabledLinksRender from "./EnabledLinksRender";
+import { type DragEndEvent, DndContext, closestCenter } from '@dnd-kit/core' ;
+import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable' ;
+import { useQueryClient } from "@tanstack/react-query";
+
+
+
+export interface linksInProfilePropsI {
+    data : userFrontI
+}
+
+export default function LinksInProfile ( {data} : linksInProfilePropsI ) {
+
+    const dataParsed = (JSON.parse(data.links!)) as socialI[] ;
+
+    const [ enabledLinks , setEnabledLinks ] = useState<socialI[]>( dataParsed.filter( (item) => item.enabled ) ) ;
+
+
+    useEffect( () => {
+
+        setEnabledLinks(dataParsed.filter( (item) => item.enabled ))
+        
+    } , [data] )
+
+
+    const queryClient = useQueryClient() ;
+
+
+    const handleRenderLinks = enabledLinks.map( (link) => {
+
+        return <EnabledLinksRender key={link.name} link={link} /> 
+
+    } )
+
+    const handleDragEnd = ( e : DragEndEvent ) => {
+
+        console.log(e.active.id) ;
+        console.log(e.over) ;
+
+        const { active, over } = e
+
+
+        if( over && over.id ) {
+
+            const prevIndex = enabledLinks.findIndex( (link) => link.id === active.id ) ;
+
+            const newIndex = enabledLinks.findIndex( (link) => link.id === over.id ) ;
+
+
+            const order = arrayMove( enabledLinks, prevIndex, newIndex )
+
+            setEnabledLinks(order) ;
+
+
+            const disabledLinks = dataParsed.filter( (item) => !item.enabled ) ; // CONSIDERAR O RECUPERAR LAS REDES SOCIALES NO HABILITADAS POR EL USUARIO, QUE NO SE PIERDAN TRAS REORDENAR LAS TARJETAS
+
+            
+            const updatedAllLinks = [ ...order, ...disabledLinks ] ;
+            
+            
+            queryClient.setQueryData( ['user'], (prevData : userFrontI) => {
+
+                return {
+                    ...prevData,
+                    links: JSON.stringify( updatedAllLinks )
+                }
+
+            } )
+            
+
+
+        }
+
+    }
+
+
+    const handleItemsSortableContext = () => enabledLinks.map( (link) => link.id! ) ;
+    
+ 
+
+    return(
+    
+        <div>
+
+            <DndContext
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
+            >
+
+            <div className='mt-20 flex flex-col gap-5' >
+
+                <SortableContext
+                    items={handleItemsSortableContext()}
+                    strategy={verticalListSortingStrategy}
+                >
+                    
+                    {handleRenderLinks}
+                
+                </SortableContext>
+
+                
+            </div>
+            
+            </DndContext>
+
+        </div>
+    
+    )
+}
