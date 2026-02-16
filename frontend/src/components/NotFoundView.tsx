846278b1b10fc3475b923dfaf37eaec6508de101
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-02-16 18:34:23 -0300

    feature: add NotFoundView component to display a user not found message

---
+++ frontend/src/components/NotFoundView.tsx
@@ -0,0 +1,12 @@
+export default function NotFoundView () {
+
+    return(
+    
+        <div>
+            <p className="font-bold text-2xl text-center text-slate-200">
+                User is not founded
+            </p>
+        </div>
+    
+    )
+}
