78253a84a1ad3dac3d70f76ac818ca1a1b9b6389
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-02-17 16:22:52 -0300

    feature: redefine the map's key as key={link.name} in the first father container div of the render map method (correct place) | refine layout and styling in DataByHandleProfileAlias component | Adjust spacing and margins for better visual appeal | Enhance link items with a consistent width and improved readability

--- frontend/src/components/DataByHandleProfileAlias.tsx
+++ frontend/src/components/DataByHandleProfileAlias.tsx
@@ -22,13 +22,13 @@ export default function DataByHandleProfileAlias ( {dataProp} : DataByHP_PropI )
 
     return(
     
-        <div className="space-y-6 text-yellow-200 p-4 flex flex-col items-center  " >
+        <div className="space-y-3 text-yellow-200 p-4 flex flex-col items-center  " >
 
-            <p className="text-center text-6xl font-semibold mb-8  shadow-lg ">
+            <p className="text-center text-6xl font-semibold mb-10  shadow-lg ">
                 {dataProp.handleProfileAlias}
             </p>
 
-            <div className="flex flex-col  w-full max-w-4xl space-y-10  md:space-y-10  md:flex-row md:justify-between md:items-start  " >
+            <div className="flex flex-col  w-full max-w-4xl space-y-10  md:space-y-0  md:flex-row md:justify-between md:items-start  " >
 
 
                 <div className="flex flex-col items-center  space-y-4  md:items-start md:w-1/2 ">
@@ -43,7 +43,7 @@ export default function DataByHandleProfileAlias ( {dataProp} : DataByHP_PropI )
 
 
                 {/*<div className="mt-20 flex flex-col gap-6" >*/}
-                <div className="shadow-sm p-2 flex gap-5 rounded-lg flex-col items-center   md:items-center md:w-1/2 "> {/*    w-full max-w-lg mx-auto" > */}
+                <div className="p-2 flex gap-5 rounded-lg flex-col items-center   md:items-center md:w-1/2 "> {/*    w-full max-w-lg mx-auto" > */}
 
                     { !isEnabledLinksVoid && enabledLinks.map( 
                         
@@ -52,25 +52,28 @@ export default function DataByHandleProfileAlias ( {dataProp} : DataByHP_PropI )
                             const iconImageDirections = `/social/icon_${link.name.toLowerCase()}${ logosPNG.includes(`${link.name.toLowerCase()}.png`) ?  '.png'  :  '.svg' }`
 
                             return(
-                                <div className="items-start flex gap-3">
-                                <h1 className="font-bold bg-stone-700      rounded-full p-2 flex items-center justify-center" >🔗</h1>
-                                <a 
-                                    key={link.name}
-                                    id={link.name}
-                                    className="min-w-[200px] px-2 py-2 flex items-start bg-emerald-600 gap-5 rounded-lg   border-2 border-emerald-700" 
-                                    href={link.url}
-                                    target="_blank"
-                                    rel="noreferrer noopener"
-                                >
+
+                                <div key={link.name} className="items-start flex gap-3 w-[360px] ">
+
+                                    <h1 className="font-bold bg-stone-700      rounded-full p-2 flex items-center justify-center" >🔗</h1>
+
+                                    <a 
+                                        
+                                        id={link.name}
+                                        className="min-w-[280px] px-2 py-2 flex items-start bg-emerald-600 gap-5 rounded-lg   border-2 border-emerald-700" 
+                                        href={link.url}
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        
+                                        <img src={iconImageDirections} alt="" className="w-12 rounded-full" />
+                                        
+                                        <p className="text-slate-900 text-lg shadow-lg" >    
+                                            My: <span className="font-bold" >{link.name}</span> 
+                                        </p> 
                                     
-                                    
-                                    <img src={iconImageDirections} alt="" className="w-12" />
-                                    
-                                    <p className="text-slate-900 text-lg " >    
-                                        My: <span className="font-bold" >{link.name}</span> 
-                                    </p> 
-                                
-                                </a>
+                                    </a>
+
                                 </div>
                             )
                         } 
